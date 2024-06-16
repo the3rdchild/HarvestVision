c16edcda23b5c87461790bfa63a9511c4a0b592e
The3rdChild <95857932+the3rdchild@users.noreply.github.com>
2024-06-16 16:06:00 +0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -14,7 +14,7 @@ pTime = 0
 
 # Set the home directory and model path
 home_directory = os.path.expanduser('~/HarvestVision')
-model_path = os.path.join(home_directory, 'best.pt') # path your model
+model_path = os.path.join(home_directory, 'data', 'HarvestVision.pt') # path your model
 result_path = os.path.join(home_directory, 'result', 'out.txt')
 image_path = os.path.join(home_directory, 'data', 'source', '1.jpg')
 result_images_path = os.path.join(home_directory, 'result', 'images')
