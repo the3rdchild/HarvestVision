9e8a581312a8dea04030cdc3530ff8def8fe9a06
The3rdChild <95857932+the3rdchild@users.noreply.github.com>
2024-11-04 01:02:04 +0700

    Update estimate.py

--- estimate/estimate.py
+++ estimate/estimate.py
@@ -2,9 +2,9 @@ import numpy as np
 import os
 import sys
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.join(current_dir, '..')
-sys.path.append(parent_dir)
+estimate_dir = os.path.dirname(os.path.abspath(__file__))
+HarvestVision = os.path.join(estimate_dir, '..')
+sys.path.append(HarvestVision)
 
 from path import csv_path, estimate_path
 
