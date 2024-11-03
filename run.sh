4bc6099dd474650b9600c7eee3a511be2d39cfa4
The3rdChild <ytcrash555@gmail.com>
2024-11-04 00:56:13 +0700

    change parent path

--- run.sh
+++ run.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
-
-path="~/HarvestVision/"
-cd $path || exit
+abshpath=$(dirname "$(realpath "$0")")
+cd "$abshpath" || exit
 
 python3 main.py &
 python3 box.py &
-python3 estimate.py &
+python3 "${abshpath}/estimate.py" &
 
 wait
