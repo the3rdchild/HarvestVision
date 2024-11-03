c52d90642b8e340bbb15d39e043fb08da02d0b70
The3rdChild <ytcrash555@gmail.com>
2024-11-04 00:47:26 +0700

    update

--- path.py
+++ path.py
@@ -1,11 +1,11 @@
 import os
 
 home_dir = os.path.dirname(os.path.abspath(__file__))
-csv_path = os.path.join(home_dir, 'result', 'Tresult.txt')
+csv_path = os.path.join(home_dir, 'Result', 'Tresult.txt')
 estimate_path = os.path.join(home_dir, 'estimate', 'estimate.txt')
 model_path = os.path.join(home_dir, 'Model', 'HarvestVision.pt')
 image_dir = os.path.join(home_dir, 'Source')
-result_path = os.path.join(home_dir, 'result', 'Result.txt')
+result_path = os.path.join(home_dir, 'Result', 'Result.txt')
 final_result_path = os.path.join(home_dir, 'result', 'Tresult.txt')
 image_path = os.path.join(home_dir, 'Source')
-image_out = os.path.join(home_dir, 'result')
+image_out = os.path.join(home_dir, 'Result')
