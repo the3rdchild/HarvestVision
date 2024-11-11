c0263009bf8bdbdb387bddc5276383f057b9f361
The3rdChild <95857932+the3rdchild@users.noreply.github.com>
2024-11-11 17:19:16 +0700

    Update box.py

--- box.py
+++ box.py
@@ -1,5 +1,4 @@
 import os
-import time
 from ultralytics import YOLO
 from path import model_path, model_path, image_path, image_out
 
@@ -7,14 +6,13 @@ model = YOLO(model_path)
 processed_images = set()
 conf_thres = 0.44
 
-while True:
-    images = [f for f in os.listdir(image_path) if f.endswith(('.png', '.jpg', '.jpeg'))]
+images = [f for f in os.listdir(image_path) if f.endswith(('.png', '.jpg', '.jpeg'))]
     
-    for image in images:
-        if image not in processed_images:
-            image_file = os.path.join(image_path, image)
+for image in images:
+    if image not in processed_images:
+        image_file = os.path.join(image_path, image)
 
-            model(image_file, 
+        model(image_file, 
                   save=True, 
                   conf=conf_thres,
                   save_crop=False, 
@@ -22,7 +20,5 @@ while True:
                   name="images", 
                   exist_ok=True)
 
-            processed_images.add(image)
-            print(f"Processed {image}")
-
-    time.sleep(10) #wait for the next image updated
+        processed_images.add(image)
+        aprint(f"Processed {image}")
